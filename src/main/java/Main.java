de4c812ef51c8c04fe9359f4ebb3db3a7d7d8716
mjaronski241132 <mjaronski241132@e-science.pl>
2021-11-25 17:17:10 +0100

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,18 @@
+import java.util.List;
+
+public class Main {
+
+
+    
+    Cars cars = new Cars();
+    public void main(String[] args) {
+        System.out.println();
+        List<Car> cars = this.cars.getCars();
+
+
+
+    }
+
+    
+    
+}
